717bd2e5de563e11b67400dfc5953b29151ae2c9
Axiom <itsaxiom.dev@proton.me>
2025-04-21 17:48:11 -0700

    need to properly set has changes on success too

--- scripts/generateMasterDuelConf.js
+++ scripts/generateMasterDuelConf.js
@@ -77,9 +77,10 @@ async function main() {
     break;
   }
 
+  core.setOutput("has_changes", changeNeeded);
+
   if (!changeNeeded) {
     core.summary.addRaw("No changes to the card list or banlist detected. Bye, Bye.").write();
-    core.setOutput("has_changes", false);
     return;
   }
 
